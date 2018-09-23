238fc8d4695a04bfcf337621523cb2dcea2095b4
Thomas Lefever-Teughels <thomas.lefever-teughels@axxes.com>
2018-09-23 20:09:10 +0200

    Pictures (from AXXES) with our own PictureStore

--- Pictures/Controllers/PicturesController.cs
+++ Pictures/Controllers/PicturesController.cs
@@ -4,16 +4,22 @@ using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Pictures.Controllers
 {
     public class PicturesController : ApiController
     {
+        // TODO: move to config
+
+        internal const string DIRECTORY = @"C:\temp\pictures\";
+        internal const string FILE_PATH = DIRECTORY + @"{0}.jpg";
+
         // GET: api/Pictures/5
         public HttpResponseMessage Get(string id)
         {
-            string filePath = string.Format(@"C:\temp\pictures\{0}.jpg", id);
+            string filePath = string.Format(FILE_PATH, id);
 
             var result = new HttpResponseMessage(HttpStatusCode.OK);
 
@@ -29,7 +35,23 @@ namespace Pictures.Controllers
                     return result;
                 }
             }
+        }
 
+        public async Task<IHttpActionResult> Post([FromUri]string fileName)
+        {
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+            var provider = new MultipartMemoryStreamProvider();
+            await Request.Content.ReadAsMultipartAsync(provider);
+            foreach (var file in provider.Contents)
+            {
+                var buffer = await file.ReadAsByteArrayAsync();
+                File.WriteAllBytes(string.Format(FILE_PATH, fileName), buffer);
+            }
+
+            return Ok();
         }
     }
 }
